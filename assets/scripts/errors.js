d2cb840136a7a8440df5d9ccf29d5819a189d2ae
Teresa Truong <truotere@gmail.com>
2021-03-16 22:22:53 -0700

    Write console functions

---
+++ assets/scripts/errors.js
@@ -0,0 +1,23 @@
+// MakeUp Lab JavaScript Error Handling and Monitoring
+
+// Error Buttons
+var logDemo = document.getElementById("log-demo");
+var errorDemo = document.getElementById("error-demo");
+var dir = document.getElementById("dir");
+var dirxml = document.getElementById("dirxml");
+var error = document.getElementById("error");
+var groupStart = document.getElementById("group-start");
+var groupEnd = document.getElementById("group-end");
+var table = document.getElementById("table");
+var startTimer = document.getElementById("start-timer");
+var endTimer = document.getElementById("end-timer");
+var trace = document.getElementById("trace");
+var globalError = document.getElementById("global-error");
+
+logDemo.onclick = function() {
+    console.log('Console Log Demo');
+}
+
+errorDemo.onclick = function() {
+    console.error('Console Error Demo');
+}
